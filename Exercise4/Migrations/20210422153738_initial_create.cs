2de5f4e320129a998f60cba2aa89d011904b373d
dell <dell@DESKTOP-SH2OB9S>
2021-04-22 23:50:30 +0800

    Add project files.

---
+++ Exercise4/Migrations/20210422153738_initial_create.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exercise4.Migrations
+{
+    public partial class initial_create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerModel",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    CheckIn = table.Column<string>(nullable: false),
+                    CheckOut = table.Column<string>(nullable: false),
+                    RoomNumber = table.Column<int>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    Billing = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerModel", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerModel");
+        }
+    }
+}
---
+++ Exercise4/Models/CustomerModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise4.Models
+{
+    public class CustomerModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm:ss}")]
+        public string CheckIn { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm:ss}")]
+        public string CheckOut { get; set; }
+
+        [Required]
+        [Range(1, 50)]
+        public int RoomNumber { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public double Billing { get; set; }
+    }
+}
